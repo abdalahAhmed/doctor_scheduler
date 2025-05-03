6f02e74b6a0bf181227fff2e4395c49d8addaae5
Abdallaht12 <af01027958926@gmail.com>
2025-05-03 14:14:27 -0700

    Add gunicorn to requirements

--- doctor_scheduler/settings.py
+++ doctor_scheduler/settings.py
@@ -30,8 +30,6 @@ ALLOWED_HOSTS = ['*']
 
 
 
-# Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -102,6 +100,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://web-production-3024.up.railway.app'
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
